45881aa817ccc9ae06760d03e24fcfdfa1b9685d
Samineitor <sanchezsamir@javeriana.edu.co>
2025-05-16 23:05:35 -0500

    🔓 Permitir carga en iframe desde frontend eliminando X-Frame-Options

---
+++ src/main/java/co/allconnected/fussiontech/apigateway/config/FrameHeaderConfig.java
@@ -0,0 +1,17 @@
+package co.allconnected.fussiontech.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.server.WebFilter;
+
+@Configuration
+public class FrameHeaderConfig {
+
+    @Bean
+    public WebFilter allowIframe() {
+        return (exchange, chain) -> {
+            exchange.getResponse().getHeaders().remove("X-Frame-Options");
+            return chain.filter(exchange);
+        };
+    }
+}
